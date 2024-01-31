2dc4594b23108cabe770af7a3480b24cdef7de42
Sanjaikabilan <sanjaikabilan.20mc@kct.ac.in>
2024-01-31 20:49:25 +0530

    add Dynamic name rendering

--- src/components/landing-page/header.tsx
+++ src/components/landing-page/header.tsx
@@ -2,8 +2,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { use, useState } from "react";
-import Logo from "@/../public/atom_logo.svg"
-import LogoDark from "@/../public/atom_logo_dark.svg"
+import Logo from "@/../public/atom_logo.svg";
+import LogoDark from "@/../public/atom_logo_dark.svg";
 
 import {
   NavigationMenu,
@@ -20,6 +20,7 @@ import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 import { ModeToggle } from "../ui/dark-mode";
 import { useTheme } from "next-themes";
+import { y_n } from "@/lib/data/constants";
 
 const routes = [
   { title: "About", href: "#about" },
@@ -37,54 +38,50 @@ const useGetTheme = () => {
   }
 };
 
-
-
-
 const Header = () => {
   const [path, setPath] = useState("#about");
   return (
     <header className=" p-4 flex justify-center items-center">
       <Link href="/" className=" w-full justify-left items-center flex gap-2 ">
         <Image src={useGetTheme()} alt="Atom Logo" width={25} height={25} />
-        <span className=" font-semibold dark:text-brand-sunglow ">sanjaikabilan<span className=" dark:text-white font-bold" >.</span></span> 
+        <span className=" font-semibold dark:text-brand-sunglow ">
+          {y_n}
+        </span>
       </Link>
 
       <NavigationMenu className="hidden md:block">
         <NavigationMenuList className="gap-6">
-        <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              About
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Project
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Blog
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                About
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Project
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Blog
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
 
       <aside className=" flex w-full gap-2 justify-end ">
-
-        
-
         <Link href={"/signup"}>
           <Button variant={"default"} className={navigationMenuTriggerStyle()}>
             Get In Touch
           </Button>
         </Link>
-        <ModeToggle/>
+        <ModeToggle />
       </aside>
     </header>
   );
