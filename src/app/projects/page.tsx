661fefc78eb13f9edbab4c98fa2c1ca335b24c1f
Sanjaikabilan <sanjaikabilan.20mc@kct.ac.in>
2024-02-02 23:41:56 +0530

    Project Table Working fine Lookin Nice

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,70 +1,45 @@
 import SectionTemplate from "@/components/landing-page/section-template";
+import ProjectTable from "@/components/projects/project-table";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { PROJECTS } from "@/lib/data/constants";
+import { ArrowUpRight } from "lucide-react";
 import React from "react";
 
 const ProjectPage = () => {
   return (
-    <section>
-      <div
-        className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col
+    <>
+      <section>
+        <div
+          className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col
         gap-4 md:justify-center md:items-center"
-      >
-        <SectionTemplate
-          title="Works of mine"
-          pill="✨ Projects"
-        ></SectionTemplate>
-        <div className=" mt-5 max-w-[950px] md:pl-5 md:pr-5">
-          <Card className=" dark:border-brand-jasmine border-brand-eirie md:w-[750px] mb-5 lg:w-[950px]">
-            <CardHeader>Project 1</CardHeader>
-            <CardContent>
-              <div className=" grid grid-flow-col grid-cols-3 gap-2 rounded-lg">
-                <div className=" col-span-1 dark:text-brand-jasmine text-sunglow-600 font-semibold">
-                  <h4>Garage Kct</h4>
-                </div>
-                <div className=" col-span-1 dark:text-brand-jasmine text-sunglow-600 font-semibold">
-                  <h4>Development</h4>
-                </div>
-                <div className=" dark:text-brand-jasmine text-sunglow-600 font-semibold justify-end items-end flex">
-                  <h4>Link</h4>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className=" dark:border-brand-jasmine border-brand-eirie md:w-[750px] mb-5 lg:w-[950px]">
-            <CardHeader>Project 1</CardHeader>
-            <CardContent>
-              <div className=" grid grid-flow-col grid-cols-3 gap-2 rounded-lg">
-                <div className=" col-span-1 dark:text-brand-jasmine text-sunglow-600 font-semibold">
-                  <h4>Garage Kct</h4>
-                </div>
-                <div className=" col-span-1 dark:text-brand-jasmine text-sunglow-600 font-semibold">
-                  <h4>Django, Jquery</h4>
-                </div>
-                <div className=" dark:text-brand-jasmine text-sunglow-600 font-semibold justify-end items-end flex">
-                  <h4>Link</h4>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className=" dark:border-brand-jasmine border-brand-eirie md:w-[750px] mb-5 lg:w-[950px]">
-            <CardHeader>Project 1</CardHeader>
-            <CardContent>
-              <div className=" grid grid-flow-col grid-cols-3 gap-2 rounded-lg">
-                <div className=" col-span-1 dark:text-brand-jasmine text-sunglow-600 font-semibold">
-                  <h4>Garage Kct</h4>
-                </div>
-                <div className=" col-span-1 dark:text-brand-jasmine text-sunglow-600 font-semibold">
-                  <h4>Django, Jquery</h4>
-                </div>
-                <div className=" dark:text-brand-jasmine text-sunglow-600 font-semibold justify-end items-end flex">
-                  <h4>Link</h4>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
+        >
+          <SectionTemplate
+            title="Works of mine"
+            pill="✨ Projects"
+          ></SectionTemplate>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <section>
+        <div
+          className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col
+        gap-4 md:justify-center md:items-center"
+        >
+          <div className=" md:max-w-[750px] w-full lg:max-w-[950px]">
+          <ProjectTable projects={PROJECTS} />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
