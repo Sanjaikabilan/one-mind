032dc99871d3cc2f05e84d36b7859b2457f4a206
Sanjaikabilan <sanjaikabilan.20mc@kct.ac.in>
2024-02-02 23:47:16 +0530

    Enhancement: Stack as Badge

--- src/components/projects/project-table.tsx
+++ src/components/projects/project-table.tsx
@@ -1,57 +1,62 @@
 import {
-    Table,
-    TableCaption,
-    TableHeader,
-    TableRow,
-    TableHead,
-    TableBody,
-    TableCell,
-  } from "@/components/ui/table"; // Import necessary components
-  import { ArrowUpRight } from 'lucide-react'; // Import ArrowUpRight icon
-  
-  interface Project {
-    p_name: string;
-    p_category: string;
-    p_stack: string;
-    preview: string;
-  }
-  
-  interface Props {
-    projects: Project[];
-  }
-  
-  const ProjectTable: React.FC<Props> = ({ projects }) => {
-    return (
-      <Table>
-        <TableCaption className="dark:bg-white bg-brand-sunglow text-black/70 dark:text-black">
-          A list of works till now
-        </TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[150px]">Project Name</TableHead>
-            <TableHead>Category</TableHead>
-            <TableHead>Stack</TableHead>
-            <TableHead className="text-right">Preview</TableHead>
+  Table,
+  TableCaption,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table"; // Import necessary components
+import { ArrowUpRight } from "lucide-react"; // Import ArrowUpRight icon
+import { Badge } from "../ui/badge";
+
+interface Project {
+  p_name: string;
+  p_category: string;
+  p_stack: string;
+  preview: string;
+}
+
+interface Props {
+  projects: Project[];
+}
+
+const ProjectTable: React.FC<Props> = ({ projects }) => {
+  return (
+    <Table>
+      <TableCaption className="dark:bg-white bg-brand-sunglow text-black/70 dark:text-black">
+        A list of works till now
+      </TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[150px]">Project Name</TableHead>
+          <TableHead>Category</TableHead>
+          <TableHead>Stack</TableHead>
+          <TableHead className="text-right">Preview</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {projects.map((project, index) => (
+          <TableRow key={index}>
+            <TableCell className="font-medium">{project.p_name}</TableCell>
+            <TableCell>{project.p_category}</TableCell>
+            <TableCell>
+              <Badge>{project.p_stack}</Badge>
+            </TableCell>
+            <TableCell className="text-right hover:underline">
+              <a
+                href={project.preview}
+                className="justify-end items-center flex"
+              >
+                <span className="">Link</span>
+                <ArrowUpRight size={18} />
+              </a>
+            </TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody>
-          {projects.map((project, index) => (
-            <TableRow key={index}>
-              <TableCell className="font-medium">{project.p_name}</TableCell>
-              <TableCell>{project.p_category}</TableCell>
-              <TableCell>{project.p_stack}</TableCell>
-              <TableCell className="text-right hover:underline">
-                <a href={project.preview} className="justify-end items-center flex">
-                  <span className="">Link</span>
-                  <ArrowUpRight size={18} />
-                </a>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    );
-  };
-  
-  export default ProjectTable;
-  
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default ProjectTable;
