30d33ea1bf7d1b63725c2e77ececbf6e4d1d893c
Sanjaikabilan <sanjaikabilan.20mc@kct.ac.in>
2024-02-10 18:08:37 +0530

    Add Skeleton

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,33 +1,15 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import SectionTemplate from "@/components/landing-page/section-template";
 import ProjectTable from "@/components/projects/project-table";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { PROJECTS } from "@/lib/data/constants";
+import { Skeleton } from "@/components/ui/skeleton";
 import { getProjects } from "@/lib/supabase/queries";
 import { Projects } from "@/lib/supabase/supabase.types";
-import { ArrowUpRight } from "lucide-react";
-import { title } from "process";
-import React, {
-  JSXElementConstructor,
-  Key,
-  PromiseLikeOfReactNode,
-  ReactElement,
-  ReactNode,
-  ReactPortal,
-  useEffect,
-  useState,
-} from "react";
 
 const ProjectPage = () => {
-  const [project, setProject] = useState<Projects[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState<Projects[]>([]);
+
   useEffect(() => {
     const fetchProjects = async () => {
       try {
@@ -36,9 +18,11 @@ const ProjectPage = () => {
           ...project,
           stack: project.stack ?? [""], // Provide a default value of an empty array if stack is null
         }));
-        setProject(transformedProjects);
+        setProjects(transformedProjects);
       } catch (error) {
         console.error("Error fetching projects:", error);
+      } finally {
+        setLoading(false); // Set loading to false once fetching is complete
       }
     };
 
@@ -48,23 +32,41 @@ const ProjectPage = () => {
   return (
     <>
       <section>
-        <div
-          className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col
-        gap-4 md:justify-center md:items-center"
-        >
-          <SectionTemplate
-            title="Works of mine"
-            pill="✨ Projects"
-          ></SectionTemplate>
+        <div className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
+          <SectionTemplate title="Works of mine" pill="✨ Projects" />
         </div>
       </section>
       <section>
-        <div
-          className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col
-        gap-4 md:justify-center md:items-center"
-        >
-          <div className=" md:max-w-[750px] w-full lg:max-w-[950px]">
-            <ProjectTable projects={project}></ProjectTable>
+        <div className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
+          <div className="md:max-w-[750px] w-full lg:max-w-[950px]">
+            {loading ? (
+              // Render Skeleton while data is being fetched
+              <div className="" >
+                <div className=" grid grid-cols-4 grid-rows-2 gap-5" >
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                </div>
+                <div className=" -mt-8 grid grid-cols-4 grid-rows-2 gap-5">
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                  <Skeleton className="h-10 " />
+                </div>
+              </div>
+            ) : (
+              // Render ProjectTable with actual data once fetching is complete
+              <ProjectTable projects={projects} />
+            )}
           </div>
         </div>
       </section>
