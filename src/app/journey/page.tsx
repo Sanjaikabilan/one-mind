17edef24c80c833450fb80cd76541cf82fdbb37e
Sanjaikabilan <sanjaikabilan.20mc@kct.ac.in>
2024-02-19 18:16:34 +0530

    Init Journey

--- src/app/journey/page.tsx
+++ src/app/journey/page.tsx
@@ -1,21 +1,26 @@
 import Header from "@/components/landing-page/header";
 import SectionTemplate from "@/components/landing-page/section-template";
 import React from "react";
+import { scroll } from "framer-motion";
 
 const JourneyPage = () => {
+  // progress equals scrollY
   return (
     <main>
       <Header />
       <section>
         <div
           className=" overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col
-      gap-4 md:justify-center md:items-center "
+                      gap-4 md:justify-center md:items-center "
         >
           <SectionTemplate
             pill="✨ Journey"
-            title=" So you&apos;re gonna hear me out "
+            title=" So you're gonna hear me out "
           ></SectionTemplate>
         </div>
+      </section>
+      <section>
+
       </section>
     </main>
   );
---
+++ src/components/journey/tracing-beam.tsx
@@ -0,0 +1,9 @@
+'use cliet'
+import { LoremIpsum } from "../global/lorem-ipsum"
+import { motion, useScroll } from "framer-motion";
+
+
+export default function Progress() {
+  const { scrollY} = useScroll();
+  
+}
